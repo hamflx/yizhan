c23c46db6fdc5905b0154b845df9a7a626918f59
hamflx <a@hamflx.cn>
2022-08-31 21:47:32 +0800

    avoid run command crash

--- packages/yizhan-node/src/commands/run.rs
+++ packages/yizhan-node/src/commands/run.rs
@@ -17,18 +17,24 @@ pub(crate) async fn do_run_command<T: Connection>(
 ) {
     let mut node_id_list = node_id.map(|id| vec![id]).unwrap_or_default();
     if node_id_list.is_empty() {
-        node_id_list.extend(conn.get_peers().await.unwrap());
+        let peers = match conn.get_peers().await {
+            Ok(v) => v,
+            Err(err) => {
+                warn!("An error occurred when get peers: {:?}", err);
+                Vec::new()
+            }
+        };
+        node_id_list.extend(peers);
     }
     if node_id_list.is_empty() {
         warn!("No target to send command");
     } else {
         let mut child = Command::new(program.as_str());
         let response = match child.output() {
-            Ok(output) => CommandRunResult::Success(
-                std::str::from_utf8(output.stdout.as_slice())
-                    .unwrap()
-                    .to_string(),
-            ),
+            Ok(output) => match std::str::from_utf8(output.stdout.as_slice()) {
+                Ok(v) => CommandRunResult::Success(v.to_string()),
+                Err(err) => CommandRunResult::Failed(format!("Err: {:?}", err)),
+            },
             Err(err) => {
                 warn!("Failed to read stdout: {:?}", err);
                 CommandRunResult::Failed(format!("Err: {:?}", err))
@@ -38,17 +44,23 @@ pub(crate) async fn do_run_command<T: Connection>(
         for node_id in node_id_list {
             if node_id != *self_node_id {
                 info!("Sending response to peer {:?}", node_id);
-                conn.send(
-                    node_id.clone(),
-                    &Message::CommandResponse(
-                        Some(node_id.clone()),
-                        cmd_id.clone(),
-                        UserCommandResponse::Run(response.clone()),
+                match conn
+                    .send(
+                        node_id.clone(),
+                        &Message::CommandResponse(
+                            Some(node_id.clone()),
+                            cmd_id.clone(),
+                            UserCommandResponse::Run(response.clone()),
+                        ),
+                    )
+                    .await
+                {
+                    Ok(_) => info!("Response sent"),
+                    Err(err) => warn!(
+                        "An error occurred when sending response to {}, {:?}",
+                        node_id, err
                     ),
-                )
-                .await
-                .unwrap();
-                info!("Response sent");
+                }
             }
         }
     }
